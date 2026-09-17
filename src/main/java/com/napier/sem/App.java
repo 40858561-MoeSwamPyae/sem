81e8b3ac8de8708da3945c6d88d90133ca1004c9
Eugene2005324 <eugene957222@gmail.com>
2026-09-17 11:17:54 +0630

    Add MongoDB driver integration to App.java

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,7 +1,36 @@
 package com.napier.sem;
 
-public class App {
-    static void main() {
-        System.out.println("Boo yah!!");
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
+public class App
+{
+    public static void main(String[] args)
+    {
+        // Connect to MongoDB using standard connection string
+        try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27000")) {
+
+            // Get database and collection
+            MongoDatabase database = mongoClient.getDatabase("mydb");
+            MongoCollection<Document> collection = database.getCollection("test");
+
+            // Create document
+            Document doc = new Document("name", "Kevin Sim")
+                    .append("class", "DevOps")
+                    .append("year", "2024")
+                    .append("result", new Document("CW", 95).append("EX", 85));
+
+            // Insert document
+            collection.insertOne(doc);
+
+            // Retrieve and print document
+            Document myDoc = collection.find().first();
+            if (myDoc != null) {
+                System.out.println(myDoc.toJson());
+            }
+        }
     }
 }
